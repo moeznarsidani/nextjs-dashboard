0170958653e9bcd7f6fa3590099e36fa9af66cb2
Moez Narsidani <Moez.Narsidani@cantire.com>
2024-10-29 14:45:15 -0400

    [UPDATE] Mutute data with Server Actions and error handling.

---
+++ app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,26 @@
+import Form from '@/app/ui/invoices/create-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
+
+export default async function Page() {
+  const customers = await fetchCustomers();
+  if (!customers) {
+    notFound();
+  }
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Create Invoice',
+            href: '/dashboard/invoices/create',
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
+  );
+}
